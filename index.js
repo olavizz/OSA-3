d09a220e7e3f46974a62d28a8200b4b055d6c22b
olavizz <149702276+olavizz@users.noreply.github.com>
2024-03-18 22:10:25 +0200

    Tehtävä 3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('data', function(req, res) {
   return JSON.stringify(req.body)
